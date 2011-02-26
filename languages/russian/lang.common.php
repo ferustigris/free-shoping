d161d614a8be6e148e34950c8c3cd4e02f10b0b8
ferus.tigris <ferus.tigris@gmail.com>
2011-02-26 17:06:44 +0000

    add to basket by js

--- languages/russian/lang.common.php
+++ languages/russian/lang.common.php
@@ -3,7 +3,8 @@
 	$this->data['Enter your name'] = 'Введите логин';
 	$this->data['Your can not view page'] = 'Вы не имеете права на просмотр';
 	$this->data['Can not open fields!'] = 'Не все поля заполнены!';
-	$this->data['Warning!'] = 'Внимание!';
+	$this->data['Warning!'] = 'Осторожно!';
+	$this->data['Attention!'] = 'Внимание!';
 	$this->data['ok'] = 'Ok';
 	$this->data['cancel'] = 'Отмена';
 ?>
