0f87bb51c90e824e0a87549cee536e19679508fb
ferus.tigris <ferus.tigris@gmail.com>
2011-02-26 21:38:13 +0000

    basket: add_product without js

--- modules/log/include/init.php
+++ modules/log/include/init.php
@@ -1,4 +1,4 @@
 <?php
 	include_once 'class.logger.php';
-	$this->set_var('log', new Logger($GLOBALS['settings']->get("log_level"), "/var/www/cms/log/messages.log"));
+	$this->set_var('log', new Logger(5, ''));//$GLOBALS['settings']->get("log_level"), "/var/www/cms/log/messages.log"));
 ?>
