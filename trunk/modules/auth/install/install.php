2a9de126fffa3221d3fb33d08184b51ede137d91
ferus.tigris@gmail.com <ferus.tigris@gmail.com@9263fb09-597a-e2dd-9a59-54a7e158e219>
2011-02-27 18:32:26 +0000

    lang: translate

--- trunk/modules/auth/install/install.php
+++ trunk/modules/auth/install/install.php
@@ -1,6 +1,6 @@
 <?php
 	$this->addPage('enter_form', 10000, 10000, 'Authorisation form', 1);
-	$this->addPage('registration_form', 0, 10000, 'Registration form', 1);
+	$this->addPage('registration_form', 10000, 10000, 'Registration form', 1);
 	$this->addPage('edit_form', 0, 9999, 'Edit form', 1);
 	$this->addPage('users', 0, 99, 'Users list', 1);
 	$this->addAction('enter', 0, 10000, 'Enter user');
