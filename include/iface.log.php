cbada7b96a2f7a5a30c2ffbec6744b591de60744
ferus.tigris <ferus.tigris@gmail.com>
2011-02-28 17:47:53 +0000

    log: LOG_INFO=5,LOG_NOTICE=6

--- include/iface.log.php
+++ include/iface.log.php
@@ -1,8 +1,8 @@
 <?php
 if(!defined('LOG_DEBUG'))define('LOG_DEBUG', 7);
 if(!defined('LOG_TODO'))define('LOG_TODO', 6);
-if(!defined('LOG_NOTICE'))define('LOG_NOTICE', 6);
-if(!defined('LOG_INFO'))define('LOG_INFO', 5);
+if(!defined('LOG_NOTICE'))define('LOG_NOTICE', 5);
+if(!defined('LOG_INFO'))define('LOG_INFO', 6);
 if(!defined('LOG_ERROR'))define('LOG_ERROR', 3);
 if(!defined('LOG_HALT'))define('LOG_HALT', 1);
 //! abstract class for log
