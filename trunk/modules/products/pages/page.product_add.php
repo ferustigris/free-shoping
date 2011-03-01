077f63e6330aa6bb81def98211be894063a06785
ferus.tigris@gmail.com <ferus.tigris@gmail.com@9263fb09-597a-e2dd-9a59-54a7e158e219>
2011-03-01 19:36:08 +0000

    menu; dynamic menu, ajax technology

--- trunk/modules/products/pages/page.product_add.php
+++ trunk/modules/products/pages/page.product_add.php
@@ -9,12 +9,13 @@
 		if(!($parent_product_id = $this->forms_get()->get('parent_product_id')))
 			$parent_product_id = -1;
 		$this->assign('parent_product', $parent_product_id);
-		$this->add_tpl('add_product_form');
 		if($mat = new Material($this, -1))
 			$this->assign('materials', $mat->all());
 		if($producer = new Producer($this, -1))
 			$this->assign('producers', $producer->all());
 		if($size = new Size($this, -1))
 			$this->assign('sizes', $size->all());
+		$this->add_tpl('add_product_form');
+		$this->add_ajax_tpl('add_product_form');
 	}
 ?>
--- trunk/modules/products/pages/page.size_add.php
+++ trunk/modules/products/pages/page.size_add.php
@@ -4,5 +4,6 @@
 	{
 		$this->assign('sizes', $size->all());
 		$this->add_tpl('add_size_form');
+		$this->add_ajax_tpl('add_size_form');
 	}
 ?>
