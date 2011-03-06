2d8388d0e499141152e4b674c58bd47018209d47
ferus.tigris@gmail.com <ferus.tigris@gmail.com@9263fb09-597a-e2dd-9a59-54a7e158e219>
2011-03-06 15:42:20 +0000

    orders: change state order

--- trunk/modules/orders/pages/page.orders.php
+++ trunk/modules/orders/pages/page.orders.php
@@ -6,7 +6,11 @@
 	{
 		$orders = $parent_order->child();
 	};
+	$states = Array();
+	if($state = new State($this, -1))
+		$states = $state->states();
 	$this->assign("list", $orders);
+	$this->assign("states", $states);
 	$this->add_tpl("orders");
 	$this->add_ajax_tpl("orders");
 ?>
