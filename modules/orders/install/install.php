b8386601a9518d7ee326edaac0e31f8b0419339b
ferus.tigris <ferus.tigris@gmail.com>
2011-03-07 09:58:20 +0000

    basket: fix: add to basket

--- modules/orders/install/install.php
+++ modules/orders/install/install.php
@@ -1,7 +1,7 @@
 <?php
 	$this->addPage('my_orders', 100, 9999, 'my orders list', 1);
 	$this->addPage('orders', 0, 999, 'orders list', 1);
-	$this->addAction('make_order', 0, 9999, 'make order');
+	$this->addAction('make_order', 0, 10000, 'make order');
 	$this->addAction('change_order', 0, 999, 'change order');
 	global $db;
 	if($db)
