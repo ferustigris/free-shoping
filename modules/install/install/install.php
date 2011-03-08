c16c230a5d09cd8a94fae0b4648414eed5ed94f2
ferus.tigris <ferus.tigris@gmail.com>
2011-03-08 13:14:17 +0000

    products: FIX: size name decode

--- modules/install/install/install.php
+++ modules/install/install/install.php
@@ -1,3 +1,7 @@
 <?php
 	if(!ISSET($GLOBALS['INDEX'])) { header('Location: /index.php'); die(); }
+	$this->addPage('install_module', 0, 100, 'install module', 1);
+	$this->addPage('modules', 0, 100, 'modules list', 1);
+	$this->addAction('install_module', 0, 100, 'install module');
+	$this->addAction('uninstall_module', 0, 100, 'uninstall module');
 ?>
--- modules/install/templates/default/tpl.module_install.php
+++
@@ -1,4 +0,0 @@
-<form action="index.php?module=<?php echo $this->name(); ?>&action=install" method="POST">
-    <input class="" type="text" name="module" value="<?php echo $this->translate('Module name'); ?>" />
-    <input id="install" type="submit" name="install" value="<?php echo $this->translate('Install'); ?>" />    
-</form>
