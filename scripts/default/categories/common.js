7e67e4168a23328bd67b5e7fe07adcb88536ed71
ferus.tigris <ferus.tigris@gmail.com>
2011-03-11 18:38:04 +0000

    fix ajax bug

--- scripts/default/categories/common.js
+++ scripts/default/categories/common.js
@@ -16,26 +16,6 @@ function onCategoryStart() {
                 primary: "ui-icon-arrowreturnthick-1-s"
             }
 	});
-	/*jQuery("#category_save_form").submit(function() {
-		jQuery('#main_page').empty() ;
-		jQuery.post("index.php?module=categories&page=categories_list&action=category_save",{
-			category_name : jQuery("#category_name") .attr("value"),
-			product_description : CKEDITOR.instances.product_description.getData(),
-			category_parent : jQuery("#category_parent option:selected") .val(),//$("#my_select option:selected").val();
-			category_img : jQuery("#category_img") .val(),
-			category_small_img : jQuery("#category_small_img") .val()
-		},
-		function(data) 
-		{
-			content = jQuery("#content", data);
-			errors = jQuery("#errors", data);
-			jQuery('#main_page').append(content.html());
-			jQuery('#top_page').empty() ;
-			jQuery('#top_page').append(errors.html());
-			onCategoryStart();
-		});
-		return false;
-	});*/	
 };
 /*! change category
  * \params
@@ -45,10 +25,7 @@ function onCategoryStart() {
 function onCategoryChange(id) 
 {
 	jQuery('#main_page').empty() ;
-	//onProductChange(-1, id);
 	jQuery.post("index.php?module=categories&page=categories_list&category_id=" + id,{
-		//producer_name: jQuery("#producer_name") .attr("value"),
-		//product_description: jQuery("#product_description") .text()
 	},
 	function(data) 
 	{
