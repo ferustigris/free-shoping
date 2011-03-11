fbe9fc62effd30fe7afcea2a58b0681f27beb0b7
ferus.tigris <ferus.tigris@gmail.com>
2011-03-11 17:35:21 +0000

    contacts: split pages message&contacts

--- modules/contacts/install/install.php
+++ modules/contacts/install/install.php
@@ -1,7 +1,9 @@
 <?php
 	if(!ISSET($GLOBALS['INDEX'])) { header('Location: /index.php'); die(); }
-	$this->addPage('contacts', 0, 10000, 'contacts', 1);
-	$this->addPage('view_contacts', 0, 1000, 'view contacts', 1);
+	$this->addPage('contacts', 1000, 10000, 'contacts', 1);
+	$this->addPage('message', 1000, 9999, 'send message to admin', 0);
+	$this->addPage('noauth', 10000, 10000, 'authorized please!', 0);
+	$this->addPage('view_contacts', 0, 999, 'view contacts', 1);
 	$this->addAction('message', 0, 9999, 'add message');
 	$this->addAction('edit', 0, 10000, 'edit contacts');
 ?>
--- modules/contacts/pages/page.contacts.php
+++ modules/contacts/pages/page.contacts.php
@@ -5,11 +5,8 @@
 	$this->assign('address', $this->options()->get('address'));
 	$this->assign('email', $this->options()->get('email'));
 	$this->assign('name', $this->options()->get('name'));
-	$authorized = false;
-	if($mod_auth = $this->get_module('auth'))
-		if($user = $mod_auth->get_var('user'))
-			$authorized = ($user->priority() < AUTH_NOBODY);
-	$this->assign('authorized', $authorized);
 	$this->add_tpl('contacts');
 	$this->add_ajax_tpl('contacts');
+	$this->load('message');
+	$this->load('noauth');
 ?>
