a2a865c4d79b14f5e2114a09ca022150e4a57227
ferus.tigris@gmail.com <ferus.tigris@gmail.com@9263fb09-597a-e2dd-9a59-54a7e158e219>
2011-03-15 10:25:36 +0000

    auth: check registr form

--- trunk/modules/auth/install/sql/users.sql
+++ trunk/modules/auth/install/sql/users.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `DB`.`PREFIXusers` (
   `id` int  NOT NULL AUTO_INCREMENT,
-  `s_name` char(30) NOT NULL COMMENT 'login',
+  `s_name` char(60) NOT NULL COMMENT 'login',
   `s_passwd` char(60) NOT NULL COMMENT 'password',
   `i_priority` int  NOT NULL COMMENT 'priority user',
   PRIMARY KEY (`id`)
