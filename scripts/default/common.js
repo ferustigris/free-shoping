4322166b3ddb66d0878e4d7a2c14ca6c95a00194
ferus.tigris <ferus.tigris@gmail.com>
2011-03-16 06:17:43 +0000

    product: js bug fix: array indexes no strings

--- scripts/default/common.js
+++ scripts/default/common.js
@@ -1,28 +1,11 @@
-/*! Создаем контейнеры-меню
+/*! load page
  * \params no
  * \return no
  */
-	$(function() {
-		$( ".column" ).sortable({
-			connectWith: ".column"
-		});
-
-		$( ".portlet" ).addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
-			.find( ".portlet-header" )
-				.addClass( "ui-widget-header ui-corner-all" )
-				.prepend( "<span class='ui-icon ui-icon-minusthick'></span>")
-				.end()
-			.find( ".portlet-content" );
-
-		$( ".portlet-header .ui-icon" ).click(function() {
-			$( this ).toggleClass( "ui-icon-minusthick" ).toggleClass( "ui-icon-plusthick" );
-			$( this ).parents( ".portlet:first" ).find( ".portlet-content" ).toggle();
-		});
-
-		//$( ".column" ).disableSelection();
+$(function() {
 		if(!isCookies())
 			$( "#nocookies" ).show();
-	});
+});
 /*! открывает эдит для ввода
  * \params 
  * - id - id element
@@ -141,7 +124,6 @@ function hideMainSection()
 {
 	$('#main_page').hide( 'slow', hideMainSectionCallback );// run the effect
 	$('#top_page').hide( 'slow', hideMainSectionCallback );// run the effect
-	//$('#main_page').toggle( 'highlight', {}, 500 );// run the effect
 };
 /*! callback function to bring a hidden box back
  * \params
