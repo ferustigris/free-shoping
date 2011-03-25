3e48452dc7179a995cbaed1a38ef515319258777
ferus.tigris <ferus.tigris@gmail.com>
2011-03-25 19:25:29 +0000

    add faq

--- modules/contacts/install/sql/messages.sql
+++ modules/contacts/install/sql/messages.sql
@@ -1,8 +1,7 @@
 
-CREATE TABLE IF NOT EXISTS `PREFIXmessages` (
+CREATE TABLE IF NOT EXISTS `PREFIXfaq` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_parent` int(11) NOT NULL COMMENT 'parent id',
-  `id_user` text NOT NULL COMMENT 'sender',
-  `s_message` text NOT NULL COMMENT 'message',
+  `s_question` text NOT NULL COMMENT 'message',
+  `s_answer` text NOT NULL COMMENT 'message',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='messages' AUTO_INCREMENT=2 ;
